6ced987a1f223f1185d269556527ab2af833186a
seanlim56 <limhyun56@gmail.com>
2026-03-09 14:55:34 +0900

    카카오 공유: urls.py에 share URL 추가

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('save/', views.save_result, name='save_result'),          # [추가됨] 저장
     path('mypage/', views.mypage, name='mypage'),
     path('detail/<int:result_id>/', views.result_detail, name='result_detail'), # [추가됨] 상세
+    path('share/<uuid:share_token>/', views.shared_result, name='shared_result'), # [카카오 공유] 공유 페이지
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
