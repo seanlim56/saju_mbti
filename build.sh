57858fe6869291fe1b66ecbd4638f7b9981c2a75
seanlim56 <limhyun56@gmail.com>
2026-03-29 17:28:50 +0900

    Fix: Remove db.sqlite3 deletion from build.sh to preserve user data on deploy
    Removed the command to delete the old database file to prevent accidental data loss during migrations.

--- build.sh
+++ build.sh
@@ -4,9 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Remove old database to avoid migration conflicts
-rm -f db.sqlite3
-
 python manage.py collectstatic --no-input
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
